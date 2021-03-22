8f261a021951c94342433131a1c06496a1f78d60
Allan Weinert <allan_weinert@hotmail.com>
2021-03-21 21:54:10 -0300

    Indentação

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -3,28 +3,32 @@ import { Injectable } from "@angular/core";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs";
 import { User } from "./user.model";
-import { Router } from '@angular/router'
+import { NavigationEnd, Router } from "@angular/router";
 
 @Injectable()
 export class LoginService {
   user: User;
 
-  constructor(private http: HttpClient,
-              private router: Router) {}
+  constructor(protected http: HttpClient, private router: Router) {}
 
   isLoggedIn(): boolean {
     return this.user !== undefined;
   }
 
   login(email: string, password: string): Observable<User> {
-    return this.http.post<User>(`${MEAT_API}/login`, {
-      email: email,
-      password: password,
-    })
-    .do(user => this.user = user);
+    return this.http
+      .post<User>(`${MEAT_API}/login`, {
+        email: email,
+        password: password,
+      })
+      .do((user) => (this.user = user));
   }
 
-  handleLogin(path?: string){
-    this.router.navigate(['/login', btoa(path)])
+  logout() {
+    this.user = undefined;
+  }
+
+  handleLogin(path?: string) {
+    this.router.navigate(["/login", btoa(path)]);
   }
 }
