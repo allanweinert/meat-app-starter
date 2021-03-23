a32027255cc7e32ddd46e7bcb9792b5a14e76800
Allan Weinert <allan_weinert@hotmail.com>
2021-03-22 21:22:40 -0300

    Finalizando event no router

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -4,12 +4,18 @@ import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs";
 import { User } from "./user.model";
 import { NavigationEnd, Router } from "@angular/router";
+import "rxjs/operator/filter";
 
 @Injectable()
 export class LoginService {
   user: User;
+  lastUrl: string;
 
-  constructor(protected http: HttpClient, private router: Router) {}
+  constructor(protected http: HttpClient, private router: Router) {
+    this.router.events.filter(e => e instanceof NavigationEnd)
+      .subscribe( (e: NavigationEnd) =>  this.lastUrl = e.url)
+    
+  }
 
   isLoggedIn(): boolean {
     return this.user !== undefined;
@@ -28,7 +34,7 @@ export class LoginService {
     this.user = undefined;
   }
 
-  handleLogin(path?: string) {
+  handleLogin(path: string = this.lastUrl) {
     this.router.navigate(["/login", btoa(path)]);
   }
 }
