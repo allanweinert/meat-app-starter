4e7c07da4e2df3b93bc27e1370e9636a5f2baeb2
Allan Weinert <allan_weinert@hotmail.com>
2021-03-23 19:51:32 -0300

    HTTP Interceptors

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -9,14 +9,12 @@ import { Order, OrderItem } from "./order.model";
 
 import { MEAT_API } from "../app.api";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { LoginService } from "app/security/login/login.service";
 
 @Injectable()
 export class OrderService {
   constructor(
     private cartService: ShoppingCartService,
-    private http: HttpClient,
-    private loginService: LoginService
+    private http: HttpClient
   ) {}
 
   itemsValue(): number {
@@ -44,15 +42,8 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    let headers = new HttpHeaders();
-    if (this.loginService.isLoggedIn()) {
-      headers = headers.set(
-        "Authorization",
-        `Bearer ${this.loginService.user.accessToken}`
-      );
-    }
     return this.http
-      .post<Order>(`${MEAT_API}/orders`, order, {headers:headers})
-      .map(order => order.id);
+      .post<Order>(`${MEAT_API}/orders`, order)
+      .map((order) => order.id);
   }
 }
